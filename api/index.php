bc86d9c442e40e61e89eb40a2b4ec0f91781718d
iZikra <zikrallahalhady@gmail.com>
2025-08-13 22:48:27 +0700

    Fix Vercel deployment configuration

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
 
-// Forward Vercel requests to normal index.php
-require __DIR__ . '/../public/index.php';
+// Definisikan direktori root aplikasi Anda
+define('LARAVEL_START', microtime(true));
+
+// Arahkan ke file bootstrap Laravel
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap aplikasi dan jalankan
+$app = require_once __DIR__.'/../bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
